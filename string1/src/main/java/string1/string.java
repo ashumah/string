9d51e05c92ca8125a456489214850fc636306301
Your Name <you@example.com>
2022-02-23 08:29:22 +0530

    sting is print

---
+++ string1/src/main/java/string1/string.java
@@ -0,0 +1,19 @@
+
+package string1;
+
+public class string {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+    {
+        String s1 = "TAT";
+        String s2 = "TAT";
+        String s3 = new String();
+        String s4 = new String();
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+    }
+}
+}
